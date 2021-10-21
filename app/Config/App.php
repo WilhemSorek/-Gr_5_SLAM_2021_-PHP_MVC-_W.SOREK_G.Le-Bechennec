92d426a593df1e19a217fee2cf9abf415f55345a
Wilhem <wilhem.sorek@gmail.com>
2021-10-21 14:53:22 +0200

    modif app

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://localhost/gsbFrameWork';
+	public $baseURL = 'http://gestfrais.gsb5.sio';
 
 	/*
 	|--------------------------------------------------------------------------
